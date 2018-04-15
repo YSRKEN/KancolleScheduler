22913a94d8be60bed59b7fae25506393260a6b7c
YSRKEN <ysr.ken@gmail.com>
2018-04-15 10:43:09 +0900

    マウスドラッグを検知して、ステータスメッセージとして表示するようにした

--- src/sample/MainModel.java
+++ src/sample/MainModel.java
@@ -1,11 +1,61 @@
 package sample;
 
+import javafx.application.Platform;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.control.Alert;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 
 /**
  * MainViewと接続されるModel
  */
 public class MainModel {
+    // publicなプロパティ
+    /**
+     * 情報表示用のテキスト
+     */
+    private ObjectProperty<String> statusMessage = new SimpleObjectProperty<>();
+    public ReadOnlyObjectProperty<String> StatusMessage = statusMessage;
+
+    // privateなプロパティ
+    /**
+     * ドラッグ開始点
+     */
+    private ObjectProperty<Double> dragStartPointX = new SimpleObjectProperty<>(0.0);
+    private ObjectProperty<Double> dragStartPointY = new SimpleObjectProperty<>(0.0);
+    /**
+     * ドラッグ途中点
+     */
+    private ObjectProperty<Double> dragMediumPointX = new SimpleObjectProperty<>(0.0);
+    private ObjectProperty<Double> dragMediumPointY = new SimpleObjectProperty<>(0.0);
+    /**
+     * ドラッグ終了点
+     */
+    private ObjectProperty<Double> dragEndPointX = new SimpleObjectProperty<>(0.0);
+    private ObjectProperty<Double> dragEndPointY = new SimpleObjectProperty<>(0.0);
+
+    // privateな処理
+    /**
+     * 情報表示用のテキストを変更する
+     */
+    private void redrawStatusMessage(){
+        Platform.runLater(() -> statusMessage.setValue(
+                String.format("マウス座標：(%d,%d)->(%d,%d)->(%d,%d)",
+                        Math.round(dragStartPointX.getValue()),
+                        Math.round(dragStartPointY.getValue()),
+                        Math.round(dragMediumPointX.getValue()),
+                        Math.round(dragMediumPointY.getValue()),
+                        Math.round(dragEndPointX.getValue()),
+                        Math.round(dragEndPointY.getValue())
+                )
+        ));
+    }
+
+    // 各種コマンド
     /**
      * 終了コマンド
      */
@@ -18,5 +68,52 @@ public class MainModel {
     public void ShowVersionInfoCommand(){
         String contentText = String.format("ソフト名：%s%nバージョン：%s", Utility.SOFT_NAME, Utility.SOFT_VERSION);
         Utility.ShowDialog(contentText, "バージョン情報", Alert.AlertType.INFORMATION);
+    }
+    /**
+     * TaskBoard上でマウスによるドラッグを開始した際のイベント
+     * @param e マウスイベント
+     */
+    public void TaskBoardDragDetected(MouseEvent e, Pane p){
+        // ドラッグ開始点の座標を記録する
+        dragStartPointX.setValue(e.getX());
+        dragStartPointY.setValue(e.getY());
+        // ドラッグイベントを許可する
+        p.startFullDrag();
+        e.consume();
+    }
+    /**
+     * TaskBoard上でマウスによるドラッグの中間状態のイベント
+     * @param e ドラッグイベント
+     */
+    public void TaskBoardMouseDragOver(MouseDragEvent e){
+        // ドラッグ途中点の座標を記録する
+        dragMediumPointX.setValue(e.getX());
+        dragMediumPointY.setValue(e.getY());
+        //
+        e.consume();
+    }
+    /**
+     * TaskBoard上でマウスによるドラッグを終了した際のイベント
+     * @param e ドラッグイベント
+     */
+    public void TaskBoardMouseDragReleased(MouseDragEvent e){
+        // ドラッグ終了点の座標を記録する
+        dragEndPointX.setValue(e.getX());
+        dragEndPointY.setValue(e.getY());
+        //
+        e.consume();
+    }
+
+    /**
+     * コンストラクタ
+     */
+    public MainModel(){
+        // マウスドラッグによるイベント処理
+        dragStartPointX.addListener((b,o,n)->redrawStatusMessage());
+        dragStartPointY.addListener((b,o,n)->redrawStatusMessage());
+        dragMediumPointX.addListener((b,o,n)->redrawStatusMessage());
+        dragMediumPointY.addListener((b,o,n)->redrawStatusMessage());
+        dragEndPointX.addListener((b,o,n)->redrawStatusMessage());
+        dragEndPointY.addListener((b,o,n)->redrawStatusMessage());
     }
 }
--- src/sample/MainViewModel.java
+++ src/sample/MainViewModel.java
@@ -1,7 +1,10 @@
 package sample;
 
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.Pane;
 
 /**
  * MainViewとMainModelとを接続するViewModel
@@ -15,6 +18,14 @@ public class MainViewModel {
      * 「バージョン情報」メニュー
      */
     @FXML private MenuItem AboutMenu;
+    /**
+     * 遠征タスクを表示するためのPane
+     */
+    @FXML private Pane TaskBoard;
+    /**
+     * 情報表示用のラベル
+     */
+    @FXML private Label StatusMessage;
 
     /**
      * MainViewと接続するモデル
@@ -25,8 +36,13 @@ public class MainViewModel {
      * ViewModelを初期化
      */
     @FXML private void initialize(){
+        // プロパティのバインディング
+        StatusMessage.textProperty().bind(mainModel.StatusMessage);
         // コマンドのバインディング
         ExitMenu.setOnAction(e -> mainModel.ExitCommand());
         AboutMenu.setOnAction(e -> mainModel.ShowVersionInfoCommand());
+        TaskBoard.setOnDragDetected(e -> mainModel.TaskBoardDragDetected(e, this.TaskBoard));
+        TaskBoard.setOnMouseDragOver(e -> mainModel.TaskBoardMouseDragOver(e));
+        TaskBoard.setOnMouseDragReleased(e -> mainModel.TaskBoardMouseDragReleased(e));
     }
 }
